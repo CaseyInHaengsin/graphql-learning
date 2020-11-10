f93f7ce759b00467dc2504fcce1a9c0e7bcf993b
Casey Rowley <casey.rowley@kuali.co>
2020-11-10 06:43:52 -0700

    updateUser mutation added.

--- graphql-basics/src/resolvers/Mutation.js
+++ graphql-basics/src/resolvers/Mutation.js
@@ -35,6 +35,33 @@ const Mutation = {
         return deletedUsers[0];
 
     },
+    updateUser(parent, args, { db }, info){
+        const { id, data } = args;
+        const user = db.users.find((user) => user.id === id)
+        
+        if (!user){
+            throw new Error("user not found")
+        }
+
+        if (typeof data.email === 'string'){
+            const emailTaken = db.users.some((user) => user.email === data.email);
+
+            if (emailTaken){
+                throw new Error('email taken')
+            }
+
+            user.email = data.email;
+        }
+
+        if (typeof data.name === 'string'){
+            user.name = data.name;
+        }
+
+        if (typeof data.age !== 'undefined'){
+            user.age = data.age;
+        }
+        return user;
+    },
     createPost(parent, args, { db }, info){
         const isPerson = db.users.some((user) => user.id === args.data.author)
         if (!isPerson){
